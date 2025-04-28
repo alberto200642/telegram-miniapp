1a8841e5269bda7090a8fe7d28210fea72fe5c8e
alberto200642 <126342421+alberto200642@users.noreply.github.com>
2025-04-28 20:46:24 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -17,6 +17,8 @@ document.getElementById("btnStart").addEventListener("click", async () => {
     if (data.success) {
         document.getElementById("pixCode").textContent = data.pixCode;
         document.getElementById("pixQrCode").src = `data:image/png;base64,${data.pixImage}`;
+        // Salvar o paymentId
+        localStorage.setItem('paymentId', data.paymentId);
     } else {
         alert("Erro ao gerar PIX. Tente novamente.");
     }
@@ -30,10 +32,14 @@ document.getElementById("copyButton").addEventListener("click", () => {
         .catch(() => alert("Erro ao copiar."));
 });
 
-// Já paguei
+// Verificar pagamento
 document.getElementById("paidButton").addEventListener("click", async () => {
-    // Aqui faria a verificação real na API ASAAS se a cobrança foi paga.
-    const response = await fetch('/check-payment');
+    const paymentId = localStorage.getItem('paymentId');
+    if (!paymentId) {
+        alert("Não foi possível localizar sua cobrança. Por favor, gere o PIX novamente.");
+        return;
+    }
+    const response = await fetch(`/check-payment/${paymentId}`);
     const data = await response.json();
 
     if (data.paymentStatus === 'RECEIVED') {
