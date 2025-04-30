8b557df8af099b61ac78461d3c46227599c04633
alberto200642 <126342421+alberto200642@users.noreply.github.com>
2025-04-29 21:24:38 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -13,6 +13,13 @@ window.onload = () => {
   const paidButton      = document.getElementById('paidButton');
   const retryButton     = document.getElementById('retryButton');
   const resetButton   = document.getElementById('resetButton');
+  
+  // Limpar o Cache
+  document.getElementById('resetButton').addEventListener('click', () => {
+    localStorage.clear();
+    location.reload();
+  });
+
 
   // Só exibe “Gerar novo PIX” se NÃO tiver recebido ainda
   if (paymentStatus !== 'RECEIVED') {
